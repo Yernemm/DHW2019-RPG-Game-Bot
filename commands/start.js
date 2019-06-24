b9fa08f11a720b4f29fc9c77da53a12da63a705c
Yernemm <yernemm@yernemm.xyz>
2019-06-25 00:19:08 +0100

    sample start

--- commands/start.js
+++ commands/start.js
@@ -17,8 +17,18 @@ module.exports = {
 //------------------------------------------------------------------------------------------------------------------------------
     // Command Logic
 
-    msg = "This command is not finished."
+    msg = ""
 
+    let sampleIntro = `\`[You wake up in an Imperial wagon driving down a snowy mountain pass. There are three other prisoners. You are seated and bound; so are the others. The one dressed in finery is gagged.]\`
+
+**[Ralof]:** *Hey, you. You're finally awake. You were trying to cross the border, right? Walked right into that Imperial ambush, same as us, and that thief over there.*`
+   data.message.channel.send(sampleIntro).then(prompt=>{
+    prompt.react('🔪')
+    .then(() => prompt.react('👄'))
+    .then(() => prompt.react('🏃'))
+    .then(() => prompt.react('❌'))
+    .catch(() => console.error('One of the emojis failed to react.'));
+  }).catch(() => console.error('Text failed to send.'));
 
 //------------------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------------    
