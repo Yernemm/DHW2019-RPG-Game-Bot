c2fb84328df132704bad9555d5aabf54e9d65225
Bryson <Pilotscottgold@gmail.com>
2019-06-24 16:39:16 -0400

    Update help.js and test.js to new template format
    .desc, .use, and .cmdtype are no longer functions. Use the properties .desc, .usage and .cmdtype or make a getter.

--- commands/help.js
+++ commands/help.js
@@ -1,141 +1,123 @@
-//METADATA
-const desc = "Shows help information"; //Short description of what the command does.
-const usage = "[command]"; //Any parameters required for command.
-const cmdtype = "help"; //Type of command
-//Command
-exports.run = (data) => {
-    // data contains: command, argsArr, argsTxt, client, message
-    //e.g. to get arguments array, use data.argsArr.
-    //--------------------------------------------------------------------
+exports = {
+    run: function (data) {
+        const { desc, usage, cmdtype } = this;
+        // data contains: command, argsArr, argsTxt, client, message
+        //e.g. to get arguments array, use data.argsArr.
 
-    //--------------------------------------------------------------------
+        //COMMAND LOGIC HERE:
 
-    //--------------------------------------------------------------------
+        //PREPARE FILESYSTEM
 
-    //COMMAND LOGIC HERE:
+        const dir = './commands/';
+        const fs = require('fs');
 
-    //PREPARE FILESYSTEM
+        //--FETCH ALL COMMAND INFO--
 
-    const dir = './commands/';
-    const fs = require('fs');
+        //DEFINE CMD CLASS
+        function cmdClass(id) {
+            this.id = id;
+            this.name = "";
+            this.desc = "";
+            this.usage = "";
+            this.type = "";
+        }
 
-    //--FETCH ALL COMMAND INFO--
+        var cmdObj = [];
+        let count = 0;
 
-    //DEFINE CMD CLASS
-    function cmdClass(id) {
-        this.id = id;
-        this.name = "";
-        this.desc = "";
-        this.usage = "";
-        this.type = "";
+        //INSTANTIATE CMD OBJECTS
+        fs.readdirSync(dir)
+            .filter(file => !file.toLowerCase().includes("template"))
+            .filter(file => file.endsWith(".js"))
+            .forEach(file => {
+                let cmdfile = require(`./${file}`);
 
+                cmdObj[count] = new cmdClass(count);
 
-    }
-    var cmdObj = [];
-    let count = 0;
+                cmdObj[count].id = count;
+                cmdObj[count].name = file.slice(0, -3);
+                cmdObj[count].desc = cmdfile.desc;
+                cmdObj[count].usage = cmdfile.usage;
+                cmdObj[count].type = cmdfile.cmdtype;
 
-    //INSTANTIATE CMD OBJECTS
-    fs.readdirSync(dir)
-      .filter(file => !file.toLowerCase().includes("template"))
-        .filter(file => file.endsWith(".js"))
-          .forEach(file => {
-            let cmdfile = require(`./${file}`);
-
-            cmdObj[count] = new cmdClass(count);
-
-            cmdObj[count].id = count;
-            cmdObj[count].name = file.slice(0, -3);
-            cmdObj[count].desc = cmdfile.desc();
-            cmdObj[count].usage = cmdfile.use();
-            cmdObj[count].type = cmdfile.cmdtype();
-
-            count++;
-          })
-
-    //CMD CLASS FUNCTIONS AND METHODS:
-
-    function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
-    }
-
-    function getAllTypes(){
-        var types = [];
-
-        cmdObj.forEach( (thisCmd) => {
-            types.push(thisCmd.type);
-        });
-        return types.filter(onlyUnique).sort();
-    }
-
-    function getAllCmdByType(type){
-        var cmds = [];
-        cmdObj.forEach( (thisCmd) => {
-            if (thisCmd.type == type)
-                cmds.push(thisCmd);
-        });
-        return cmds;
-    }
-
-
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
-    var msg = "error";
-    switch (data.argsTxt) {
-        case null:
-        case "":
-        case " ":
-
-            msg = `**Discord Hack Week 2019  RPG Game Bot\r\nTo get help for a specific command, do ${data.config.prefix}help [command]\r\n\r\n`;
-
-
-            msg += "Commands:\r\n"
-
-
-            var cmdInfoMsg = "";
-            console.log(getAllTypes());
-            getAllTypes().forEach( (type)=>{
-                cmdInfoMsg+= "**" + capitalizeFirstLetter(type) + "**\r\n```";
-                getAllCmdByType(type).forEach( (tcmd) =>{
-                    cmdInfoMsg += `${tcmd.name} `;
-                });
-                cmdInfoMsg+= "```\r\n";
-
+                count++;
             });
 
-            msg += "\r\n" + cmdInfoMsg;
+        //CMD CLASS FUNCTIONS AND METHODS:
 
-            break;
+        function onlyUnique(value, index, self) {
+            return self.indexOf(value) === index;
+        }
+
+        function getAllTypes(){
+            var types = [];
+
+            cmdObj.forEach( (thisCmd) => {
+                types.push(thisCmd.type);
+            });
+            return types.filter(onlyUnique).sort();
+        }
+
+        function getAllCmdByType(type){
+            var cmds = [];
+            cmdObj.forEach( (thisCmd) => {
+                if (thisCmd.type == type)
+                    cmds.push(thisCmd);
+            });
+            return cmds;
+        }
+
+
+        function capitalizeFirstLetter(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+
+        var msg = "error";
+        switch (data.argsTxt) {
+            case null:
+            case "":
+            case " ":
+
+                msg = `**Discord Hack Week 2019  RPG Game Bot\r\nTo get help for a specific command, do ${data.config.prefix}help [command]\r\n\r\n`;
+
+
+                msg += "Commands:\r\n"
+
+
+                var cmdInfoMsg = "";
+                console.log(getAllTypes());
+                getAllTypes().forEach( (type)=>{
+                    cmdInfoMsg+= "**" + capitalizeFirstLetter(type) + "**\r\n```";
+                    getAllCmdByType(type).forEach( (tcmd) =>{
+                        cmdInfoMsg += `${tcmd.name} `;
+                    });
+                    cmdInfoMsg+= "```\r\n";
+
+                });
+
+                msg += "\r\n" + cmdInfoMsg;
+
+                break;
             default:
 
-            let cmd = data.argsTxt;
-            try {
-                let file = require(`./${cmd}.js`);
+                let cmd = data.argsTxt;
+                try {
+                    let file = require(`./${cmd}.js`);
 
-                let de = file.desc();
-                let us = file.use();
-                msg = `**Help for ${data.config.prefix}${cmd}**\r\n${de}\r\nUsage: ${data.config.prefix}${cmd} ${us}`;
-            } catch (err) {
-                msg = `Command \"${data.argsTxt}\" not found.\r\nUse **>help** to see available commands.`
-            }
+                    let de = file.desc;
+                    let us = file.use;
+                    msg = `**Help for ${data.config.prefix}${cmd}**\r\n${de}\r\nUsage: ${data.config.prefix}${cmd} ${us}`;
+                } catch (err) {
+                    msg = `Command \"${data.argsTxt}\" not found.\r\nUse **>help** to see available commands.`
+                }
 
-            break;
+                break;
+        }
 
-
-    }
-
-
-    //--------------------------------------------------------------------
-data.message.channel.send(msg);
-   return msg;
-}
-exports.desc = () =>{
-    return desc;
-}
-exports.use = () =>{
-    return usage;
-}
-exports.cmdtype = () => {
-    return cmdtype;
+        data.message.channel.send(msg);
+        return msg;
+    },
+    desc: "Shows help information",
+    usage: "[command]",
+    cmdtype: "help",
 }
--- commands/test.js
+++ commands/test.js
@@ -1,32 +1,23 @@
-//METADATA
-const desc = "A first testing command."; //Short description of what the command does.
-const usage = ""; //Any parameters required for command.
-const cmdtype = "test"; //Type of command
-//Command
-exports.run = (data) => {
-    // data contains: command, argsArr, argsTxt, client, message
-    //e.g. to get arguments array, use data.argsArr.
-    //--------------------------------------------------------------------
+exports = {
+    run: function (data) {
+        // data contains: command, argsArr, argsTxt, client, message
+        //e.g. to get arguments array, use data.argsArr.
+        //--------------------------------------------------------------------
 
-    //--------------------------------------------------------------------
-    let msg = "" //Message to return to user (will be logged)
-    //--------------------------------------------------------------------
+        //--------------------------------------------------------------------
+        let msg = "" //Message to return to user (will be logged)
+        //--------------------------------------------------------------------
 
-    //COMMAND LOGIC HERE:
+        //COMMAND LOGIC HERE:
 
-    msg = "hewwo worl owo";
+        msg = "hewwo worl owo";
 
 
-    //--------------------------------------------------------------------
-   data.message.channel.send(msg);
-   return msg;
-};
-exports.desc = () =>{
-    return desc;
-};
-exports.use = () =>{
-    return usage;
-};
-exports.cmdtype = () => {
-    return cmdtype;
+        //--------------------------------------------------------------------
+        data.message.channel.send(msg);
+        return msg;
+    },
+    desc: "A first testing command.",
+    usage: "",
+    cmdtype: "test"
 };
