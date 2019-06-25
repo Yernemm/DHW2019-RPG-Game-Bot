09ff50d3de9fc07b438655af6905cad4a53621ae
Bryson <Pilotscottgold@gmail.com>
2019-06-24 21:41:17 -0400

    Update startprompts.js

--- commands/startprompts.js
+++ commands/startprompts.js
@@ -5,7 +5,7 @@ var usage = ""
 var cmdtype = "game"
 //-------------------------
 
-const prompts = require('../modules/prompt.js');
+const { Prompt, Choice } = require('../modules/prompt.js');
 const emojis = require('../modules/emojis.js');
 
 module.exports = {
@@ -22,19 +22,49 @@ module.exports = {
     // MAY NOT WORK, May need to be rewritten entirely
     let player = data.message.author;
 
-    let scene4 = new prompts.Prompt(4, 'You reached the end, congratulations!', []).save();
+    let choice6to2 = new prompts.Choice(2, emojis.b, 'Run away', true);
     let choice2to4 = new prompts.Choice(4, emojis.a, 'Proceed to your destination', true);
     let choice7to4 = new prompts.Choice(4, emojis.a, 'Proceed to your destination', true);
-    let scene7 = new prompts.Prompt(7, 'You defeated the bandit', [choice7to4]).save();
-    let choice6to7 = new prompts.Choice(7, emojis.a, 'Win the fight', true);
-    let scene8 = new prompts.Prompt(8, 'You are defeated by the bandit, you are dead', []).save();
     let choice6to8 = new prompts.Choice(8, emojis.b, 'Lose the fight', true);
+    let choice6to7 = new prompts.Choice(7, emojis.a, 'Win the fight', true);
+    
+    let scene4 = new prompts.Prompt(4, 'You reached the end, congratulations!', []).save();
+
+    let scene7 = new prompts.Prompt(7, 'You defeated the bandit', [choice7to4]).save();
+
+    let scene8 = new prompts.Prompt(8, 'You are defeated by the bandit, you are dead', []).save();
+
     let scene2 = new prompts.Prompt(2, 'You are traveling down a dirt road', [choice2to4]).save();
-    let choice6to2 = new prompts.Choice(2, emojis.b, 'Run away', true);
+    
     let scene6 = new prompts.Prompt(6, 'See a bandit in front of you, he wants to fight.', [choice6to7, choice6to8]).save();
+
+
+
     let choice1to2 = new prompts.Choice(2, emojis.a, 'Take the road', true);
     let choice1to6 = new prompts.Choice(6, emojis.b, 'Go through the woods', true);
     let scene1 = new prompts.Prompt(1, 'You are leaving a town, you have the feeling you need to keep traveling.', [choice1to2], [choice1to6]).save();
+
+    Prompt.save(1, 'You are leaving a town, you have the feeling you need to keep traveling.', [
+      new Choice(2, emojis.a, 'Take the road'),
+      new Choice(6, emojis.b, 'Go through the woods')
+    ]);
+
+    Prompt.save(2, 'You are traveling down a dirt road', [
+      new Choice(4, emojis.a, 'Proceed to your destination')
+    ]);
+
+    Prompt.save();
+
+    Prompt.save(6, 'See a bandit in front of you, he wants to fight.', [
+      new Choice(7, emojis.a, 'Win the fight'),
+      new Choice(8, emojis.b, 'Lose the fight')
+    ]);
+
+    Prompt.save(7, 'You defeated the bandit', [
+
+    ]);
+
+
 
     scene1.display(data.message.channel);
     msg = scene1.formatted;
