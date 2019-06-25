7bb07b98a60914134f5afa3e2fa55f40c3e46a88
Bryson <Pilotscottgold@gmail.com>
2019-06-24 21:26:11 -0400

    Update game.js

--- modules/game.js
+++ modules/game.js
@@ -1,5 +1,5 @@
-var { GameUser, Player, userList } = require('./GameUser.js');
-var { Prompt, Choice, promptList } = require('./Prompt.js');
+var { GameUser, Player, userList } = require('./gameUser.js');
+var { Prompt, Choice, promptList } = require('./prompt.js');
 var emojiList = require('./emojis.js');
 
 module.exports.onReact = function onReact(messageReaction, user) {
