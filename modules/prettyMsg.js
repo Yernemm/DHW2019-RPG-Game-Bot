d3e13d2ba99bf2b5f5530834729a3924328e7435
811Alex <811alex@gmail.com>
2019-06-27 16:39:21 +0300

    Fix bug

--- modules/prettyMsg.js
+++ modules/prettyMsg.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 class PrettyMsg extends Discord.RichEmbed {
   constructor(message, player = null){
-    super({title: message});
+    super({description: message});
     if(player) this.setAuthor(player.username, player.avatarURL);
   }
 }
