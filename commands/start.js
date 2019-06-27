62885da06c9cf1446bb780185a7cc184ad3e720d
Yernemm <yernemm@yernemm.xyz>
2019-06-27 14:48:22 +0100

    rename startprompts to start

--- commands/start.js
+++ commands/start.js
@@ -1,16 +1,24 @@
+const game = require('../modules/game.js');
+
 module.exports = {
   // METADATA
   desc: "Starts the game.",
   usage: "",
   cmdtype: "game",
-  run: data => {
+  run: async data => {
     // data contains: command, argsArr, argsTxt, client, message, config
     //e.g. to get arguments array, use data.argsArr.
 
-    // Command Logic
-    let msg = "This command is not finished."; // Message to return to user (will be logged)
+    // COMMAND LOGIC
+
+    let msg = ""; // Message to return to user (will be logged)
+
+    // MAY NOT WORK, May need to be rewritten entirely
+    //let player = data.message.author;
 
 
-    data.message.channel.send(msg);
+    msg = await game.start(data.message);
+
+    //data.message.channel.send(msg);
     return msg;
 }};
