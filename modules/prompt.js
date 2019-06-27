d4fdb32cdc277d32815c41c3359bbb21e93d6c75
811Alex <811alex@gmail.com>
2019-06-27 07:28:03 +0300

    bug fix

--- modules/prompt.js
+++ modules/prompt.js
@@ -194,7 +194,7 @@ class Prompt {
     await emojis.reduce((lastPromise, emoji) => {
       return lastPromise.then(() => msg.react(emoji));
     }, Promise.resolve());
-    await msg.createReactionCollector((reaction, user) => user.id !== client.user.id).collected.forEach((reaction) => reaction.remove()); // remove reactions before we're ready
+    await msg.createReactionCollector((reaction) => !reaction.me).collected.forEach((reaction) => reaction.remove()); // remove extra reactions before we're ready
     return msg;
   }
 
